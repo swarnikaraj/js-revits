198d46d69a57abe708e9e035c4cb5358e28376c3
Swarnnika Raj Singh <swarnikarajsingh@gmail.com>
2024-03-29 16:41:43 +0530

    remove  a word

--- subset.js
+++ subset.js
@@ -1,6 +1,5 @@
 // remove al the a from a string
 
-let str1 = "sedaadrvafesa";
 function helperRemoveA(initstr, str) {
   if (str == "") {
     return initstr;
@@ -24,9 +23,24 @@ function helperRemoveA2(str) {
     return lastchar + helperRemoveA2(substr);
   }
 }
-function removeA() {
-  let ans = "";
 
-  console.log(helperRemoveA2(str1));
+function removeTiger(str) {
+  if (str == "") {
+    return "";
+  }
+  lastchar = str[0];
+  if (str.startsWith("tiger")) {
+    let substr = str.substring(5);
+    return removeTiger(substr);
+  } else {
+    let substr = str.substring(1);
+    return lastchar + removeTiger(substr);
+  }
+}
+function removeA() {
+  let str1 = "sedaadrvafesa";
+  let str2 = "adsfslfmtigersdsl";
+  //   console.log(helperRemoveA2(str1));
+  console.log(removeTiger(str2));
 }
 removeA();
